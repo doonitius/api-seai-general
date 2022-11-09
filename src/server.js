17738e5ed2c414930828dc4ead6b2bf3a49e6767
doonitius <dkitsakulpaisan@gmail.com>
2022-11-10 00:01:15 +0700

    initial project

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const server = http.createServer(app)
+
+app.get("/health", (req, res) => {
+    res.status(200).send("api-seai-general is running fine")
+})
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+    console.log("API Server is running on port" + port)
+    console.log("go to http://localhost:" + port + "/health")
+})
