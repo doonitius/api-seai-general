290a909c5d973c8233a43b666d97122aba5010c4
doonitius <dkitsakulpaisan@gmail.com>
2022-11-11 01:50:31 +0700

    set up mongodb

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const http = require('http')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+require('dotenv').config()
+
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -10,6 +16,15 @@ const server = http.createServer(app)
 app.get("/health", (req, res) => {
   res.status(200).send("api-seai-general is running fine")
 })
+
+mongoose.connect(
+  process.env.DB_CLUSTER,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log('general: connected to database')
+)
 
 const port = process.env.PORT || 8000;
 
