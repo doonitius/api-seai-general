af05d59ae8a7ab3fec4d578a792db57b8a7d5ce3
doonitius <dkitsakulpaisan@gmail.com>
2023-05-11 19:37:34 +0700

    turn off secure route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.use('/auth', require('./auth_route'))
-router.use(require('./secure_route'))
+// router.use(require('./secure_route'))
 router.use('/project', require('./thesis_project_route'))
 router.use('/advisor', require('./advisor_route'))
 router.use('/keyword', require('./keyword_route'))
